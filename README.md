3e92e0f0acd98f424f331cdadcb859bd571be74e
KirianOlle <kirian@altair.com>
2025-04-02 19:50:35 +0200

    seventh commit

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # snippetbox
 
 To start the app
+```bash
+go run /usr/local/go/src/crypto/tls/generate_cert.go --rsa-bits=2048 --host=localhost
+```
 
 ```go
 go run ./cmd/web [>>/tmp/info.log 2>>/tmp/error.log]
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"net/http"
+	"time"
 )
 
 func init() {
@@ -13,15 +15,23 @@ func init() {
 func main() {
 	flag.Parse()
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr:     Config.addr,
-		ErrorLog: App.errorLog,
-		Handler:  App.routes(),
+		Addr:         Config.addr,
+		ErrorLog:     App.errorLog,
+		Handler:      App.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	App.infoLog.Println("Starting server on ", Config.addr)
 
-	err := srv.ListenAndServe()
+	err := srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 
 	App.errorLog.Fatal(err)
 }
